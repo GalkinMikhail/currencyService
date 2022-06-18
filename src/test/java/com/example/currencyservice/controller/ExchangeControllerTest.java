ec86bf0adec10a7339901c55324b68cfa761f784
GalkinMikhail <galkin101@yandex.ru>
2022-06-18 16:37:06 +0300

    test fixes

--- src/test/java/com/example/currencyservice/controller/ExchangeControllerTest.java
+++ src/test/java/com/example/currencyservice/controller/ExchangeControllerTest.java
@@ -1,44 +1,128 @@
 package com.example.currencyservice.controller;
 
-import org.junit.Before;
-import org.junit.Test;
+import com.example.currencyservice.service.implement.GifServiceImpl;
+import com.example.currencyservice.service.implement.RatesServiceImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-public class ExchangeControllerTest {
+@RunWith(SpringRunner.class)
+@WebMvcTest(ExchangeController.class)
+class ExchangeControllerTest {
+
+    @Value("${gif.rich}")
+    private String richTag;
+    @Value("${gif.broke}")
+    private String brokeTag;
+    @Value("${gif.zero}")
+    private String zero;
+    @Value("${gif.error}")
+    private String errorTag;
+
 
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private RatesServiceImpl ratesService;
+    @MockBean
+    private GifServiceImpl gifService;
+
+
     @Test
-    public void getCurrencyList() throws Exception {
+    void getCurs() throws Exception {
+        List<String> responseList = new ArrayList<>();
+        responseList.add("TEST");
+        Mockito.when(ratesService.getCurrencies())
+                .thenReturn(responseList);
         mockMvc.perform(get("/api/currencies")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0]").value("TEST"));
+    }
+
+    @Test
+    public void getGifRich() throws Exception {
+        Map<String, String> map = new HashMap<>();
+        map.put("result", this.richTag);
+        ResponseEntity<Map> responseEntity = new ResponseEntity<>(map, HttpStatus.OK);
+        Mockito.when(ratesService.getKey(anyString()))
+                .thenReturn(1);
+        Mockito.when(gifService.getGif(this.richTag))
+                .thenReturn(responseEntity);
+        mockMvc.perform(get("/api/gif/TESTCODE")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andDo(print())
-                .andExpect(status().isOk());
+                .andExpect(jsonPath("$.result").value(this.richTag));
     }
 
     @Test
-    public void getGif() throws Exception {
-        mockMvc.perform(get("/api/gif/RUB")
+    public void getGifBroke() throws Exception {
+        Map<String, String> map = new HashMap<>();
+        map.put("result", this.brokeTag);
+        ResponseEntity<Map> responseEntity = new ResponseEntity<>(map, HttpStatus.OK);
+        Mockito.when(ratesService.getKey(anyString()))
+                .thenReturn(-1);
+        Mockito.when(gifService.getGif(this.brokeTag))
+                .thenReturn(responseEntity);
+        mockMvc.perform(get("/api/gif/TESTCODE")
                         .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andDo(print());
+                .andDo(print())
+                .andExpect(jsonPath("$.result").value(this.brokeTag));
     }
 
+    @Test
+    public void getGifZero() throws Exception {
+        Map<String, String> map = new HashMap<>();
+        map.put("result", this.zero);
+        ResponseEntity<Map> responseEntity = new ResponseEntity<>(map, HttpStatus.OK);
+        Mockito.when(ratesService.getKey(anyString()))
+                .thenReturn(0);
+        Mockito.when(gifService.getGif(this.zero))
+                .thenReturn(responseEntity);
+        mockMvc.perform(get("/api/gif/TESTCODE")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(jsonPath("$.result").value(this.zero));
+    }
+
+    @Test
+    public void getGifError() throws Exception {
+        Map<String, String> map = new HashMap<>();
+        map.put("result", this.errorTag);
+        ResponseEntity<Map> responseEntity = new ResponseEntity<>(map, HttpStatus.OK);
+        Mockito.when(ratesService.getKey(anyString()))
+                .thenReturn(404);
+        Mockito.when(gifService.getGif(this.errorTag))
+                .thenReturn(responseEntity);
+        mockMvc.perform(get("/api/gif/TESTCODE")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(jsonPath("$.result").value(this.errorTag));
+    }
 }
